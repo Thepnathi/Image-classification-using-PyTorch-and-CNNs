9e23bf58c6a5ab14cb696fac7c68f9766ec16c23
Thepnathi <thepnathi@hotmail.com>
2021-01-05 12:00:44 +0000

    Update model_accuracy.py to work with dataset

--- model_accuracy.py
+++ model_accuracy.py
@@ -1,7 +1,8 @@
-import torch
+import torch as th
 from cnn import ConvolutionalNetwork
+from train_network import train_dataset, test_dataset
 
-# Test the ndetwork against the test dataset
+device = torch.device("cuda" if torch.cuda.is_available() else "gpu")
 
 class Model_Accuracy(object):
     def __init__(self, net):
@@ -11,30 +12,33 @@ class Model_Accuracy(object):
         net = self.net.to(device)
         correct_prediction = 0
         total = 0
-        
-        for images, labels in data_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = net(images)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0) 
-            correct_prediction += (predicted == labels).sum()
+
+        for i in range(len(data_loader)):
+          input = th.tensor([data_loader[i]['imNorm']])
+          label = th.tensor([data_loader[i]['label']])
+          input, label = input.to(device), label.to(device)
+          outputs = self.net(input)
+          _, predicted = th.max(outputs, 1)
+          total += label.size(0)
+          correct_prediction += (predicted == labels).sum()
         accuracy = 100 * float(correct_prediction) / total
         print('Accuracy of the network on the {} {} images: {:2f} %'.format(total, name, accuracy))
 
     def train_set_accuracy(self, train_loader):
-        dataset_accuracy(train_loader, "train")
+        self.dataset_accuracy(train_loader, "train")
 
     def val_set_accuracy(self, val_loader):
-        dataset_accuracy(val_loader, "validation")
+        self.dataset_accuracy(val_loader, "validation")
 
     def test_set_accuracy(self, test_loader):
-        dataset_accuracy(test_loader, "test")
+        self.dataset_accuracy(test_loader, "test")
 
-    def compute_accuracy(self, train_loader, val_loader, test_loader):
-        train_set_accuracy(train_loader)
-        val_set_accuracy(val_loader)
-        test_set_accuracy(test_loader)
+    def compute_accuracy(self, train_loader, test_loader):
+        self.train_set_accuracy(train_loader)
+        # self.val_set_accuracy(val_loader)
+        self.test_set_accuracy(test_loader)
 
-if __name__ == "__main__":
-    net = ConvolutionalNetwork()
-    model = Model_Accuracy(net)
+net = ConvolutionalNetwork()
+model = Model_Accuracy(net)
+train_dataset[0]['label']
+model.compute_accuracy(train_dataset, test_dataset)
